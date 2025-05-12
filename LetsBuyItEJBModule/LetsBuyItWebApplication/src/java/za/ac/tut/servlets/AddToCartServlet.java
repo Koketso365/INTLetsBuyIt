e978cb0e3319294ca3bad42488b522308400f88b
Koketso365 <thekokoketsoprecious@gmail.com>
2025-05-12 12:05:13 +0200

    Add to Cart Updated

--- LetsBuyItEJBModule/LetsBuyItWebApplication/src/java/za/ac/tut/servlets/AddToCartServlet.java
+++ LetsBuyItEJBModule/LetsBuyItWebApplication/src/java/za/ac/tut/servlets/AddToCartServlet.java
@@ -7,11 +7,21 @@ package za.ac.tut.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import za.ac.tut.bl.ProductFacade;
+import za.ac.tut.bl.ProductFacadeLocal;
+import za.ac.tut.entities.Product;
+import za.ac.tut.entities.User;
+import za.ac.tut.holder.CartManager;
 
 /**
  *
@@ -55,10 +65,43 @@ public class AddToCartServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    @EJB
+    private ProductFacadeLocal pfl;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+
+        User user = (User) session.getAttribute("user");
+        String productIdStr = request.getParameter("productId");
+
+        if (productIdStr == null) {
+            response.sendRedirect("ViewProductsServlet.do");
+            return;
+        }
+
+        try {
+            Long productId = Long.parseLong(productIdStr);
+            Product product = pfl.find(productId);
+
+            if (product != null && product.getQuantity() > 0) {
+                // Add to cart (Session-based cart)
+                List<Product> cart = CartManager.getCart(session);
+                CartManager.addToCart(cart, product);
+
+            }
+
+        } catch (NumberFormatException e) {
+            // Log error if needed
+        }
+        
+        RequestDispatcher dispatcher =request.getRequestDispatcher("browseProducts.jsp");
+        dispatcher.forward(request, response);
     }
 
     /**
